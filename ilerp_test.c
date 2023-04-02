3b9a538afddf5e19c2aa8ac2ce98ef64dbf08950
Maik Steiger <fifeimcity@gmail.com>
2023-04-02 22:38:58 +0200

    Improved "tests"

--- ilerp_test.c
+++ ilerp_test.c
@@ -1,36 +1,63 @@
-//
-// Created by Walera on 02.04.2023.
-//
+/**
+ * This file was created by HumansAreWeak (https://github.com/HumansAreWeak)
+ *
+ * It is part of the ilerp library. The crude "unit tests" of the library are written here.
+ *
+ * @author HumansAreWeak
+ * @date 02.04.2023
+ */
 #include <assert.h>
 #include "ilerp.h"
 
 void test_ilerps_base()
 {
-    assert(_ilerps_base(0, 255, 0, uint8_t) == 0);
-    assert(_ilerps_base(0, 255, 64, uint8_t) == 127);
-    assert(_ilerps_base(0, 255, 128, uint8_t) == 255);
+    /** --- _ilerps_u8 Tests --- */
+    assert(_ilerps_u8(0, 255, 0) == 0);
+    assert(_ilerps_u8(0, 255, 64) == 127);
+    assert(_ilerps_u8(0, 255, 128) == 255);
 
-    assert(_ilerps_base(0, 42, 0, uint8_t) == 0);
-    assert(_ilerps_base(0, 42, 64, uint8_t) == 21);
-    assert(_ilerps_base(0, 42, 128, uint8_t) == 42);
+    assert(_ilerps_u8(0, 42, 0) == 0);
+    assert(_ilerps_u8(0, 42, 64) == 21);
+    assert(_ilerps_u8(0, 42, 128) == 42);
 
     for(uint32_t i = 0; i <= 128; i++)
     {
-        assert(_ilerps_base(0, 20, i, uint8_t) <= 20);
-        assert(_ilerps_base(10, 20, i, uint8_t) <= 20);
-        assert(_ilerps_base(10, 20, i, uint8_t) >= 10);
+        assert(_ilerps_u8(10, 20, i) <= 20);
+        assert(_ilerps_u8(10, 20, i) >= 10);
+    }
+
+    /** --- _ilerps_i8 Tests --- */
+    assert(_ilerps_i8(0, 127, 0) == 0);
+    assert(_ilerps_i8(0, 127, 64) == 63);
+    assert(_ilerps_i8(0, 127, 128) == 127);
+
+    assert(_ilerps_i8(0, 42, 0) == 0);
+    assert(_ilerps_i8(0, 42, 64) == 21);
+    assert(_ilerps_i8(0, 42, 128) == 42);
+
+    for(uint32_t i = 0; i <= 128; i++)
+    {
+        assert(_ilerps_i8(-10, 20, i) <= 20);
+        assert(_ilerps_i8(-10, 20, i) >= -10);
     }
 }
 
 void test_ilerpd_base()
 {
-    assert(_ilerpd_base(0, 255, 0, uint8_t) == 0);
-    assert(_ilerpd_base(0, 255, 128, uint8_t) == 128);
-    assert(_ilerpd_base(0, 255, 255, uint8_t) == 255);
+    /** --- _ilerpd_u8 Tests --- */
+    assert(_ilerpd_u8(0, 255, 0) == 0);
+    assert(_ilerpd_u8(0, 255, 128) == 128);
+    assert(_ilerpd_u8(0, 255, 255) == 255);
 
-    assert(_ilerpd_base(0, 42, 0, uint8_t) == 0);
-    assert(_ilerpd_base(0, 42, 128, uint8_t) == 21);
-    assert(_ilerpd_base(0, 42, 255, uint8_t) == 42);
+    assert(_ilerpd_u8(0, 42, 0) == 0);
+    assert(_ilerpd_u8(0, 42, 128) == 21);
+    assert(_ilerpd_u8(0, 42, 255) == 42);
+
+    for(uint32_t i = 0; i <= 255; i++)
+    {
+        assert(_ilerpd_u8(10, 20, i) <= 20);
+        assert(_ilerpd_u8(10, 20, i) >= 10);
+    }
 }
 
 // Entry point
